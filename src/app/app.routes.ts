2cbd41fd7fff22b8a4ab599636f0fd37452b468f
Gerardo Palacin <gerardopalacin.github@gmail.com>
2026-09-24 10:51:15 -0500

    feat(layout-pnf): add layout and page not found configuration and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import {Home} from './shared/presentation/views/home/home';
 const baseTitle = 'ACME Learning Center';
 const about = () => import('./shared/presentation/views/about/about').then(m =>m.About);
+const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found')
+  .then((m) => m.PageNotFound);
+
 
 export const routes: Routes = [
   {path: 'home', component: Home, title:`${baseTitle} - Home`},
-  {path: 'about', loadComponent:about, title:`${baseTitle} - About`}
+  {path: 'about', loadComponent:about, title:`${baseTitle} - About`},
+  {path: '',redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` }
 
 ];
