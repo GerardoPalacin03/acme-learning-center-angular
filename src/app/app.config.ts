3210167842bb0bdef57c49d99beb54bbfd1ab7e8
Gerardo Palacin <gerardopalacin.github@gmail.com>
2026-09-24 09:46:40 -0500

    feat(i18n): add language configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
+import {provideTranslateService} from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
+    provideTranslateService({
+      loader:provideTranslateHttpLoader({
+        prefix:'./i18n/',
+        suffix:'.json'
+      }),
+      fallbackLang:'en',
+    }),
     provideRouter(routes)
   ]
 };
